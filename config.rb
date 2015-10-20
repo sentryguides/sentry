13af269ef42b535f20f35d83906553d5bef0635d
Jakob Gillich <jakob@gillich.me>
2015-10-21 00:03:15 +0200

    fix build

--- config.rb
+++ config.rb
@@ -1,6 +1,4 @@
-require "lib/custom_helpers"
 require "lib/rendering_hack"
-
 
 activate :blog do |blog|
   blog.prefix = "blog"
@@ -33,18 +31,22 @@ helpers do
     "<!--SENTRY_CONTENT_SEPARATOR-->"
   end
 
+  def markdown(source)
+    Tilt[:markdown].new { source }.render
+  end
+
   def ability(id, options = {})
     a = data[:abilities][id]
-    img = app.image_url "/images/dota/abilities/#{id}.png"
+    img = "/images/dota/abilities/#{id}.png"
 
     tooltip = %Q(
       <img src="#{img}" width="32" height="32"> #{a["dname"]}</b>
       <br>#{a["desc"]}
-      <br>#{a["dmg"]}#{if a["attrib"].empty? then "" else ability["attrib"] + "<br>" end}#{a["cmb"]}
-    )
-    html = %Q(
-      <a href="/abilities/#{id}.html" class="tooltip-link" data-toggle="tooltip" data-html="true" data-placement="bottom" title='#{tooltip}'>
-    )
+      <br>#{a["dmg"]}#{if a["attrib"].empty? then "" else a["attrib"] + "<br>" end}#{a["cmb"]}
+    ).gsub("'", "&#39;").gsub("\r", "").gsub("\n", "")
+
+    html = %Q(<a href="/abilities/#{id}.html" class="tooltip-link" data-toggle="tooltip" data-html="true" data-placement="bottom" title='#{tooltip}'>)
+
     if options[:icon]
       options[:width] = options[:height] if options[:width] == :auto
       options[:height] = options[:width] if options[:height] == :auto
@@ -59,24 +61,24 @@ helpers do
 
   def item(id, options = {})
     i = data[:items][id]
-    img = app.image_url "/images/dota/items/#{id}.png"
+    img = "/images/dota/items/#{id}.png"
 
     tooltip = %Q(
-    <img src="#{img}" width="42.24" height="32"> #{i["dname"]}
-    #{if i["desc"].empty? then "" else "<br>" + i["desc"] end}
-    #{if i["attrib"].empty? then "" else "<br>" + i["attrib"] end}
-    )
-    html = %Q(
-      <a href="/items/#{id}.html" class="tooltip-link" data-toggle="tooltip" data-html="true" data-placement="bottom" title='#{tooltip}'>
-    )
+      <img src="#{img}" width="42.24" height="32"> #{i["dname"]}
+      #{if i["desc"].empty? then "" else "<br>" + i["desc"] end}
+      #{if i["attrib"].empty? then "" else "<br>" + i["attrib"] end}
+    ).gsub("'", "&#39;").gsub("\r", "").gsub("\n", "")
+
+    html = %Q(<a href="/items/#{id}.html" class="tooltip-link" data-toggle="tooltip" data-html="true" data-placement="bottom" title='#{tooltip}'>)
+
     if options[:icon]
-      options[:width] = options[:height] if options[:width] == :auto
-      options[:height] = options[:width] if options[:height] == :auto
+      options[:width] = options[:height] * 1.32 if options[:width] == :auto
+      options[:height] = options[:width] / 1.32 if options[:height] == :auto
       html += %Q(
-        <img src="#{img}" width="#{options[:width] || 32}" height="#{options[:height] || 32}">
+        <img src="#{img}" width="#{options[:width] || 32}" height="#{options[:height] || 32}" style="object-fit: cover">
       )
     else
-      html += %Q(<img src="#{img}" height="16"> #{a["dname"]})
+      html += %Q(<img src="#{img}" height="16" style="object-fit: cover"> #{i["dname"]})
     end
     html + "</a>"
   end
